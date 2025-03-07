8c42a8f9967862c891dcad47e0d247e2651e7855
mashin-84 <t0884@slsh.ntpc.edu.tw>
2025-03-07 17:34:45 +0800

    修正 PWA 安裝功能 & 新增 script.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,26 @@
-self.addEventListener("install", function(event) {
-    console.log("Service Worker 安裝完成");
-    event.waitUntil(
-        caches.open("pwa-budget-app").then(function(cache) {
-            return cache.addAll(["/", "/index.html", "/styles.css"]);
-        })
-    );
+const CACHE_NAME = "pwa-accounting-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
